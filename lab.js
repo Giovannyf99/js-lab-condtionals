f3fb1776c6ba4175dd790782baa65c31b15dac84
Giovannyf99 <giovannyf1999@gmail.com>
2023-03-08 14:49:01 -0500

    inital commit

--- lab.js
+++ lab.js
@@ -9,7 +9,15 @@ const prompt = require('prompt-sync')({sigint: true});
   All user input will be read as a String, so in order to treat user input as numbers, you’ll need to convert the input:
 */
 
-
+const number = prompt('what is your favorite number?')
+if(number == 0){
+  console.log("you entered zero")
+} else if(number > 0){
+  console.log("you entered a positive number")
+} else(number < 0){
+  console.log("you entered a negative number")
+}
+console.log(`${number}`)
 
 
 
